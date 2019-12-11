535c4cb461d916ae513e6a786817e7ac9502f07c
marcoantoniogsilva <50424217+marcoantoniogsilva@users.noreply.github.com>
2019-12-11 18:43:44 -0300

    Exercício: While2

---
+++ Exerc2-While/Exerc2-While/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exerc2_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("y: ");
+            int y = int.Parse(Console.ReadLine());
+
+            while (x != 0 && y != 0)
+            {
+                if(x > 0 && y > 0)
+                {
+                    Console.WriteLine("Primeiro");
+                }
+                else if(x < 0 && y > 0)
+                {
+                    Console.WriteLine("Segundo");
+                }
+                else if(x < 0 && y < 0)
+                {
+                    Console.WriteLine("Terceiro");
+                }
+                else
+                {
+                    Console.WriteLine("Quarto");
+                }
+                Console.Write("x: ");
+                x = int.Parse(Console.ReadLine());
+
+                Console.Write("y: ");
+                y = int.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
