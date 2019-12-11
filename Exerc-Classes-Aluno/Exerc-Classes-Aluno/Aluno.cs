1a1aea97ad8aaa5bf91058185e7d77c74e9f86c7
marcoantoniogsilva <50424217+marcoantoniogsilva@users.noreply.github.com>
2019-12-11 18:45:33 -0300

    Exercício: Classes-Aluno

---
+++ Exerc-Classes-Aluno/Exerc-Classes-Aluno/Aluno.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exerc_Classes_Aluno
+{
+    class Aluno
+    {
+        public string Nome;
+        public double N1, N2, N3;
+
+        public double NotaFinal()
+        {
+            return N1 + N2 + N3;
+        }
+
+        public bool VerificarSePassou()
+        {
+            if(NotaFinal() >= 60.0)
+            {
+                return true;
+            }
+                return false;
+        }
+
+        public double NotaRestante()
+        {
+            if(VerificarSePassou() == false)
+            {
+                return 60.0 - NotaFinal();
+            }
+            else
+            {
+                return 0.0; 
+            }
+        }
+    }
+}
---
+++ Exerc-Classes-Aluno/Exerc-Classes-Aluno/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exerc_Classes_Aluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a1 = new Aluno();
+
+            Console.Write("Digite o nome do aluno: ");
+            a1.Nome = Console.ReadLine();
+
+            Console.Write("Digite as três notas do aluno: ");
+            a1.N1 = double.Parse(Console.ReadLine());
+            a1.N2 = double.Parse(Console.ReadLine());
+            a1.N3 = double.Parse(Console.ReadLine());
+
+            a1.NotaFinal();
+
+            Console.WriteLine("NOTA FINAL: " + a1.NotaFinal());
+            
+            if(a1.VerificarSePassou() == true)
+            {
+                Console.WriteLine("APROVADO!");
+            }
+                Console.WriteLine("REPROVADO!");
+                Console.WriteLine("FALTARAM " + a1.NotaRestante() + " PONTOS.");
+        }
+    }
+}
