3aa553514d94b441db04f2051dc98653d9056ddc
marcoantoniogsilva <50424217+marcoantoniogsilva@users.noreply.github.com>
2019-12-11 18:42:53 -0300

    Exercício: Classes2

---
+++ Exerc2-Classes/Exerc2-Classes/Funcionario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exerc2_Classes
+{
+    class Funcionario
+    {
+        public string nome;
+        public double salario;
+    }
+}
---
+++ Exerc2-Classes/Exerc2-Classes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exerc2_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1, f2;
+            f1 = new Funcionario();
+            f2 = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            f1.nome = Console.ReadLine();
+
+            Console.Write("Salário: ");
+            f1.salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            f2.nome = Console.ReadLine();
+
+            Console.Write("Salário: ");
+            f2.salario = double.Parse(Console.ReadLine());
+
+            double media = (f1.salario + f2.salario) / 2.0;
+            Console.WriteLine("Salário médio: " + media.ToString("F2"));
+        }
+    }
+}
