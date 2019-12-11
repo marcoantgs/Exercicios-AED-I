32997e6f8120f99d7469b2c28b79a9f9fb00daae
marcoantoniogsilva <50424217+marcoantoniogsilva@users.noreply.github.com>
2019-12-11 18:55:37 -0300

    Exercício: Classes-Retangulo

---
+++ Exerc-Classes-Retangulo/Exerc-Classes-Retangulo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exerc_Classes_Retangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+
+            Console.Write("Entre com a largura e altura do retângulo: ");
+            r.Largura = double.Parse(Console.ReadLine());
+            r.Altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("ÁREA = " + r.Area());
+            Console.WriteLine("PERÍMETRO = " + r.Perimetro());
+            Console.WriteLine("DIAGONAL = " + r.Diagonal());
+        }
+    }
+}
---
+++ Exerc-Classes-Retangulo/Exerc-Classes-Retangulo/Retangulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exerc_Classes_Retangulo
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Altura * Largura;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (Largura + Largura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Altura * Altura + Largura * Largura);
+        }
+    }
+}
