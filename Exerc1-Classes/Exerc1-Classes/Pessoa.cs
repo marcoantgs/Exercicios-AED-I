7c07e4878df26cf730974e1b92a935bc42b1068c
marcoantoniogsilva <50424217+marcoantoniogsilva@users.noreply.github.com>
2019-12-11 18:41:41 -0300

    Exercício: Classes1

---
+++ Exerc1-Classes/Exerc1-Classes/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exerc1_Classes
+{
+    class Pessoa
+    {
+        public string nome;
+        public int idade;
+    }
+}
---
+++ Exerc1-Classes/Exerc1-Classes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exerc1_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1, p2;
+            p1 = new Pessoa();
+            p2 = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            p1.nome = Console.ReadLine();
+
+            Console.Write("Idade: ");
+            p1.idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.Write("Nome: ");
+            p2.nome = Console.ReadLine();
+
+            Console.Write("Idade: ");
+            p2.idade = int.Parse(Console.ReadLine());
+
+            string maior;
+
+            if (p1.idade > p2.idade)
+            {
+                maior = p1.nome;
+            }
+            else
+            {
+                maior = p2.nome;
+            }
+
+            Console.WriteLine("Pessoa mais velha: " + maior);
+        }
+    }
+}
